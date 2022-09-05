58acbb380db96449a8c4094dcd28f9a971a509a4
Joan Casimero <111473276+joancasimero@users.noreply.github.com>
2022-09-05 10:23:13 +0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) {
+
+        Student joan = new Student();
+
+        joan.surname = "Casimero";
+        joan.firstName = "Joan";
+        joan.middleInitial = "D";
+        joan.dateOfBirth = 01/29/2004;
+        joan.studentNumber = 2022100892;
+        joan.studentEmailAddress = "casimerojd@students.national-u.edu.ph";
+        joan.iAmAwesome = true;
+        joan.sayMyStudentNumber();
+        joan.sayMystudentEmailAddress();
+        joan.AmIAwesome();
+
+    }
+
+}
+
+
+
+       
